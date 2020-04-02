aff045bd0f53bea2b618646cffc8280173ae21b7
L3if <zagnetin.lev@gmail.com>
2020-04-03 01:20:13 +0300

    added breadcrumb nav

--- LR2Boosted/js/tableScriptLR2.js
+++ LR2Boosted/js/tableScriptLR2.js
@@ -52,6 +52,35 @@ initialForm.style.width = "70%";
 initialForm.classList.add("mx-auto");
 document.body.appendChild(initialForm);
 main();
+document.getElementById("nav_container").appendChild(createBreadcrumb());
+
+function createBreadcrumb()
+{
+    let breadcrumb = document.createElement('nav');
+    breadcrumb.setAttribute('aria-label', 'breadcrumb');
+    let breadcrumbOl = document.createElement('ol');
+    breadcrumbOl.classList.add('breadcrumb');
+    pathArr = window.location.pathname.split("/");
+    for (let i = 1; i<pathArr.length; i++)
+    {
+        if (i<pathArr.length-1)
+        {
+            let currPathItem = document.createElement('li');
+            currPathItem.classList.add('breadcrumb-item');
+            currPathItem.innerText = pathArr[i];
+            breadcrumbOl.appendChild(currPathItem);
+        } else
+        {
+            let currPathItem = document.createElement('li');
+            currPathItem.classList.add('breadcrumb-item');
+            currPathItem.classList.add('active');
+            currPathItem.innerText = pathArr[i];
+            breadcrumbOl.appendChild(currPathItem);
+        }
+    }
+    breadcrumb.appendChild(breadcrumbOl);
+    return breadcrumb
+}
 
 function goToParamPage()
 {
